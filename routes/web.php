47aa18cbbbc01f82b0783cea4afa4a83ff3ef8d9
R. Alvian Putra Mahardhika <alvian.rapm@gmail.com>
2019-06-19 14:37:11 +0700

    Templating dan Login

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin','middleware'=>'auth','namespace'=>'admin'], function(){
+    Route::get('dashboard','DashboardController@index')->name('admin.dashboard');
+});
